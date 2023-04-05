c87a2ecf726147bd22ec7d52aa63847e1f49acfc
xadupre <xadupre@microsoft.com>
2023-04-06 01:31:08 +0200

    Adds a function to measure AVX instructions

--- _unittests/ut_validation/test_vector_sum.py
+++ _unittests/ut_validation/test_vector_sum.py
@@ -1,7 +1,13 @@
 import unittest
 import numpy
 from onnx_extended.ext_test_case import ExtTestCase
-from onnx_extended.validation._validation import vector_sum
+from onnx_extended.validation._validation import (
+    vector_sum,
+    vector_sum_array,
+    vector_sum_array_parallel,
+    vector_sum_array_avx,
+    vector_sum_array_avx_parallel,
+)
 
 
 class TestVectorSum(ExtTestCase):
@@ -19,15 +25,35 @@ class TestVectorSum(ExtTestCase):
 
     def test_vector_sum_array(self):
         values = numpy.array([10, 1, 4, 5, 6, 7], dtype=numpy.float32)
-        t1 = vector_sum(1, values, True)
-        t2 = vector_sum(1, values, False)
+        t1 = vector_sum_array(1, values, True)
+        t2 = vector_sum_array(1, values, False)
         self.assertEqual(t1, 33)
         self.assertEqual(t2, 33)
 
-        t1 = vector_sum(2, values, True)
-        t2 = vector_sum(2, values, False)
+        t1 = vector_sum_array(2, values, True)
+        t2 = vector_sum_array(2, values, False)
         self.assertEqual(t1, 33)
         self.assertEqual(t2, 33)
+
+    def test_vector_sum_array_parallel(self):
+        values = numpy.arange(16 * 16).reshape((-1, 16)).astype(numpy.float32)
+        t = values.sum()
+        t1 = vector_sum_array_parallel(16, values, True)
+        t2 = vector_sum_array_parallel(16, values, False)
+        self.assertEqual(t, t1)
+        self.assertEqual(t, t2)
+
+    def test_vector_sum_array_avx(self):
+        values = numpy.arange(16 * 16).reshape((-1, 16)).astype(numpy.float32)
+        t = values.sum()
+        t1 = vector_sum_array_avx(16, values)
+        self.assertEqual(t, t1)
+
+    def test_vector_sum_array_avx_parallel(self):
+        values = numpy.arange(16 * 16).reshape((-1, 16)).astype(numpy.float32)
+        t = values.sum()
+        t1 = vector_sum_array_avx_parallel(16, values)
+        self.assertEqual(t, t1)
 
 
 if __name__ == "__main__":
