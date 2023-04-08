b48bbf288a0914abc0ab6d274f6eebcefb2739fc
xavier dupré <xavier.dupre@gmail.com>
2023-04-08 12:31:50 +0200

    Adds a skeleton for a function able to add two vectors

--- _unittests/ut_validation/test_vector_sum.py
+++ _unittests/ut_validation/test_vector_sum.py
@@ -2,6 +2,7 @@ import unittest
 import numpy
 from onnx_extended.ext_test_case import ExtTestCase
 from onnx_extended.validation._validation import (
+    vector_add,
     vector_sum,
     vector_sum_array,
     vector_sum_array_parallel,
@@ -54,6 +55,22 @@ class TestVectorSum(ExtTestCase):
         t = values.sum()
         t1 = vector_sum_array_avx_parallel(16, values)
         self.assertEqual(t, t1)
+
+    def test_vector_add_exc(self):
+        # This test checks function vector_add
+        # raises an exception if the dimension do not match.
+        v1 = numpy.ones((3, 4), dtype=numpy.float32)
+        v2 = numpy.ones((4,), dtype=numpy.float32)
+        self.assertRaise(lambda: vector_add(v1, v2), RuntimeError)
+        v2 = numpy.ones((4, 3), dtype=numpy.float32)
+        self.assertRaise(lambda: vector_add(v1, v2), RuntimeError)
+
+    def test_vector_add(self):
+        v1 = numpy.ones((3, 4), dtype=numpy.float32)
+        v2 = numpy.ones((3, 4), dtype=numpy.float32)
+        v3 = vector_add(v1, v2)
+        self.assertEqual(v3.shape, (3, 4))
+        # Adds some code to check the addition is ok.
 
 
 if __name__ == "__main__":
