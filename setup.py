fe54f3dc37666637a572549bb55c55e48da873c0
xadupre <xadupre@microsoft.com>
2023-04-17 10:38:56 +0200

    wheel

--- setup.py
+++ setup.py
@@ -296,7 +296,6 @@ setup(
         "Operating System :: Unix",
         "Operating System :: MacOS",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
