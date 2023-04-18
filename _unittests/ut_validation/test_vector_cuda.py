c4ddf3ac454ab72910f2f8e64ea404d064b1ed41
xadupre <xadupre@microsoft.com>
2023-04-18 16:21:20 +0200

    add cuda

--- _unittests/ut_validation/test_vector_cuda.py
+++ _unittests/ut_validation/test_vector_cuda.py
@@ -1,15 +1,27 @@
 import unittest
 import numpy
 from onnx_extended.ext_test_case import ExtTestCase
-from onnx_extended.validation.cuda_example_py import vector_sum
+from onnx_extended import has_cuda
+if has_cuda():
+    from onnx_extended.validation.cuda_example_py import vector_sum
+else:
+    vector_sum = None
 
 
 class TestVectorCuda(ExtTestCase):
+    @unittest.skipIf(vector_sum is None, reason="CUDA not available")
+    def test_cuda_version(self):
+        from onnx_extended import cuda_version
+        self.assertTrue(has_cuda())
+        self.assertNotEmpty(cuda_version())
+
+    @unittest.skipIf(vector_sum is None, reason="CUDA not available")
     def test_vector_cuda(self):
         values = numpy.array([[10, 1, 4, 5, 6, 7]], dtype=numpy.float32)
         t = vector_sum(values)
         self.assertEqual(t, values.sum())
 
+    @unittest.skipIf(vector_sum is None, reason="CUDA not available")
     def test_vector_cuda_big(self):
         values = numpy.random.randn(3, 224, 224).astype(numpy.float32)
         t = vector_sum(values)
--- onnx_extended/__init__.py
+++ onnx_extended/__init__.py
@@ -5,3 +5,21 @@ More operator for onnx reference implementation.
 
 __version__ = "0.1.0"
 __author__ = "Xavier Dupré"
+
+
+def has_cuda():
+    """
+    Tells if cuda is available.
+    """
+    from ._config import HAS_CUDA
+    return HAS_CUDA == 1
+
+
+def cuda_version():
+    """
+    Tells which version of CUDA was used to build the CUDA extensions.
+    """
+    if not has_cuda():
+        raise RuntimeError("CUDA extensions are not available.")
+    from ._config import CUDA_VERSION
+    return CUDA_VERSION
