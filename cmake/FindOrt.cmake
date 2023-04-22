a2e723eb90ca80f79689fc0bbb27f680da3def51
xadupre <xadupre@microsoft.com>
2023-04-22 17:46:26 +0200

    lint

--- cmake/FindOrt.cmake
+++ cmake/FindOrt.cmake
@@ -4,10 +4,6 @@
 # downloads onnxruntime as a binary
 #
 
-#
-# 
-#
-
 if(NOT ORT_VERSION)
   set(ORT_VERSION 1.14.1)
 endif()
@@ -35,35 +31,6 @@ set(ORT_ROOT "https://github.com/microsoft/onnxruntime/releases/download/")
 set(ORT_URL "${ORT_ROOT}v${ORT_VERSION}/${ORT_NAME}")
 set(ORT_DEST "${CMAKE_CURRENT_BINARY_DIR}/onnxruntime-download/${ORT_NAME}")
 set(ORT_DEST_DIR "${CMAKE_CURRENT_BINARY_DIR}/onnxruntime-bin/")
-
-# if(EXISTS "${ORT_DEST}")
-#   message(STATUS "onnxruntime - already downloaded '${ORT_DEST}'")
-# else()
-#   message(STATUS "onnxruntime - download '${ORT_URL}' into '${ORT_DEST}'")
-#   file(DOWNLOAD "${ORT_URL}" "${ORT_DEST}" SHOW_PROGRESS)
-# endif()
-# file(SIZE "${ORT_DEST}" ORT_DEST_SIZE)
-# if(ORT_DEST_SIZE LESS 1024)
-#   message(FATAL_ERROR "Empty file, unable to download '${ORT_URL}'.")
-# endif()
-# if(EXISTS "${ORT_DEST}")
-#  if(EXISTS "${ORT_DEST_DIR}/${ORT_FOLD}/VERSION_NUMBER")
-#   message(STATUS "onnxruntime - extracted '${ORT_DEST}'")
-#   else()
-#     message(STATUS "onnxruntime - extract '${ORT_DEST}'")
-#     file(ARCHIVE_EXTRACT INPUT "${ORT_DEST}" DESTINATION "${ORT_DEST_DIR}" TOUCH VERBOSE)
-#   endif()
-# else()
-#   message(FATAL_ERROR "Unable to download '${ORT_URL}'.")
-# endif()
-# if(EXISTS "${ORT_DEST_DIR}/${ORT_FOLD}/VERSION_NUMBER")
-#   message(STATUS "onnxruntime - found ${ORT_DEST_DIR}/${ORT_FOLD}/VERSION_NUMBER'.")
-# else()
-#   message(FATAL_ERROR "Unable to extract '${ORT_DEST}'.")
-# endif()
-# set(ORT_DIR "${ORT_DEST_DIR}${ORT_FOLD}")
-# add_library(onnxruntime SHARED IMPORTED)
-# set_target_properties(onnxruntime PROPERTIES IMPORTED_LOCATION "${ORT_DIR}/lib/libonnxruntime.so")
 
 FetchContent_Declare(onnxruntime URL ${ORT_URL})
 FetchContent_makeAvailable(onnxruntime)
