e3849dcf50afa3f55e3783f421cd745808dad931
xadupre <xadupre@microsoft.com>
2023-05-14 12:58:03 +0200

    update gemm implementation

--- _unittests/test_common.h
+++ _unittests/test_common.h
@@ -45,3 +45,18 @@ bool check_equal(int n, T* pa, T* pb) {
 }
 
 #define ASSERT_EQUAL_VECTOR(n, pa, pb) ASSERT_THROW(check_equal(n, pa, pb))
+
+template <typename T>
+bool check_almost_equal(int n, T* pa, T* pb, T precision=1e-5) {
+    for (int i = 0; i < n; ++i) {
+        if (pa[i] == pb[i])
+            continue;
+        T d = pa[i] - pb[i];
+        d = d > 0 ? d : -d;
+        if (d > precision)
+            return false;
+    }
+    return true;
+}
+
+#define ASSERT_ALMOST_VECTOR(n, pa, pb, prec) ASSERT_THROW(check_almost_equal(n, pa, pb, prec))
--- onnx_extended/reference/c_ops/cpu/c_op_conv_common.h
+++ onnx_extended/reference/c_ops/cpu/c_op_conv_common.h
@@ -11,101 +11,108 @@ namespace onnx_c_ops {
 // was initialized.
 template <typename NTYPE>
 void gemm(bool transA, bool transB, size_t M, size_t N, size_t K, NTYPE alpha,
-          const NTYPE *A, const NTYPE *B, NTYPE beta, NTYPE *C) {
+		  const NTYPE* A, const NTYPE* B, NTYPE beta, NTYPE* C) {
 
-  typedef Map<Matrix<NTYPE, Dynamic, Dynamic>> matrixdd;
-  if (transA) {
-    if (transB) {
-      matrixdd ma((NTYPE*)A, M, K);
-      matrixdd mb((NTYPE*)B, K, N);
-      matrixdd mc(C, M, N);
-      if (beta != 1)
-        mc *= beta;
-      mc += (ma.transpose() * ma.transpose()) * alpha;
-      return;
-    }
-    else {
-      matrixdd ma((NTYPE*)A, M, K);
-      matrixdd mb((NTYPE*)B, K, N);
-      matrixdd mc(C, M, N);
-      if (beta != 1)
-        mc *= beta;
-      mc += (ma.transpose() * mb) * alpha;
-      return;
-    }
-  }
-  else if(transB) {
-      matrixdd ma((NTYPE*)A, M, K);
-      matrixdd mb((NTYPE*)B, K, N);
-      matrixdd mc(C, M, N);
-      if (beta != 1)
-        mc *= beta;
-      mc += (ma * mb.transpose()) * alpha;
-      return;
-  }
-  else {
-      matrixdd ma((NTYPE*)A, M, K);
-      matrixdd mb((NTYPE*)B, K, N);
-      matrixdd mc(C, M, N);
-      if (beta != 1)
-        mc *= beta;
-      mc += (ma * mb) * alpha;
-      return;
-  }
+	typedef Map<Matrix<NTYPE, Dynamic, Dynamic, RowMajor>> matrixdd_row;
+	typedef Map<Matrix<NTYPE, Dynamic, Dynamic, ColMajor>> matrixdd_col;
+	matrixdd_row mc(C, M, N);
+	if (beta != 1)
+		mc *= beta;
+	if (transA) {
+		matrixdd_col ma((NTYPE*)A, M, K);
+		if (transB) {
+			matrixdd_col mb((NTYPE*)B, K, N);
+			if (alpha != 1)
+				mc.noalias() += alpha * ma * mb;
+			else
+				mc.noalias() += ma * mb;
+			return;
+		}
+		else {
+			matrixdd_row mb((NTYPE*)B, K, N);
+			if (alpha != 1)
+				mc.noalias() += alpha * ma * mb;
+			else
+				mc.noalias() += ma * mb;
+			return;
+		}
+	}
+	else {
+		matrixdd_row ma((NTYPE*)A, M, K);
+		if (transB) {
+			matrixdd_col mb((NTYPE*)B, K, N);
+			if (alpha != 1)
+				mc.noalias() += alpha * ma * mb;
+			else
+				mc.noalias() += ma * mb;
+			return;
+		}
+		else {
+			matrixdd_row mb((NTYPE*)B, K, N);
+			if (alpha != 1)
+				mc.noalias() += alpha * ma * mb;
+			else
+				mc.noalias() += ma * mb;
+			return;
+		}
+	}
 
-  if (transA) {
-    if (transB) {
-    } else {
-      // a A B + b C, dimension = M * N
-      NTYPE *begin;
-      NTYPE val;
-      NTYPE val0;
-      size_t i, j, k, maxc = 0;
-      const NTYPE *pA, *pB;
-      for (i = 0, begin = C; i < M; ++i) {
-        for (j = 0; j < N; ++j, ++begin) {
-          val0 = *begin * beta;
-          val = 0;
-          pA = A + i;
-          pB = B + j;
-          for (k = K; k > 0; --k, pA += K, pB += N)
-            val += *pA * *pB;
-          *begin = val0 + val * alpha;
-          maxc = maxc > (size_t)(begin - C) ? maxc : (size_t)(begin - C);
-          if (maxc > M * N)
-            throw std::invalid_argument("gemm10: maxc > M * N");
-        }
-      }
-      return;
-    }
-  } else {
-    if (transB) {
-    } else {
-      // a A B + b C, dimension = M * N
-      NTYPE *begin;
-      NTYPE val;
-      NTYPE val0;
-      size_t i, j, k, maxc = 0;
-      const NTYPE *pA, *pB;
-      for (i = 0, begin = C; i < M; ++i) {
-        for (j = 0; j < N; ++j, ++begin) {
-          val0 = *begin * beta;
-          val = 0;
-          pA = A + i * K;
-          pB = B + j;
-          for (k = K; k > 0; --k, ++pA, pB += N)
-            val += *pA * *pB;
-          *begin = val0 + val * alpha;
-          maxc = maxc > (size_t)(begin - C) ? maxc : (size_t)(begin - C);
-          if (maxc > M * N)
-            throw std::invalid_argument("gemm00: maxc > M * N");
-        }
-      }
-      return;
-    }
-  }
-  throw std::invalid_argument(
-      "Not implemented for transposed matrices (Gemm<T>).");
+	if (transA) {
+		if (transB) {
+		}
+		else {
+			// a A B + b C, dimension = M * N
+			NTYPE* begin;
+			NTYPE val;
+			NTYPE val0;
+			size_t i, j, k, maxc = 0;
+			const NTYPE* pA, * pB;
+			for (i = 0, begin = C; i < M; ++i) {
+				for (j = 0; j < N; ++j, ++begin) {
+					val0 = *begin * beta;
+					val = 0;
+					pA = A + i;
+					pB = B + j;
+					for (k = K; k > 0; --k, pA += K, pB += N)
+						val += *pA * *pB;
+					*begin = val0 + val * alpha;
+					maxc = maxc > (size_t)(begin - C) ? maxc : (size_t)(begin - C);
+					if (maxc > M * N)
+						throw std::invalid_argument("gemm10: maxc > M * N");
+				}
+			}
+			return;
+		}
+	}
+	else {
+		if (transB) {
+		}
+		else {
+			// a A B + b C, dimension = M * N
+			NTYPE* begin;
+			NTYPE val;
+			NTYPE val0;
+			size_t i, j, k, maxc = 0;
+			const NTYPE* pA, * pB;
+			for (i = 0, begin = C; i < M; ++i) {
+				for (j = 0; j < N; ++j, ++begin) {
+					val0 = *begin * beta;
+					val = 0;
+					pA = A + i * K;
+					pB = B + j;
+					for (k = K; k > 0; --k, ++pA, pB += N)
+						val += *pA * *pB;
+					*begin = val0 + val * alpha;
+					maxc = maxc > (size_t)(begin - C) ? maxc : (size_t)(begin - C);
+					if (maxc > M * N)
+						throw std::invalid_argument("gemm00: maxc > M * N");
+				}
+			}
+			return;
+		}
+	}
+	throw std::invalid_argument(
+		"Not implemented for adjointd matrices (Gemm<T>).");
 }
 
 }; // namespace onnx_c_ops
