95c511487db87872e1644ab16914e82741d9bd4c
xadupre <xadupre@microsoft.com>
2023-05-21 08:52:51 +0200

    rename a file

--- _unittests/ut_ortcy/test_ortcy_classes.py
+++ _unittests/ut_ortcy/test_ortcy_classes.py
@@ -2,11 +2,11 @@ import unittest
 from onnx_extended.ext_test_case import ExtTestCase
 
 
-class TestOrtCyValue(ExtTestCase):
-    def test_api(self):
-        from onnx_extended.ortcy.wrap.ortinf import Shape
+class TestOrtCyClasses(ExtTestCase):
+    def test_ort_shape(self):
+        from onnx_extended.ortcy.wrap.ortinf import OrtShape
 
-        shape = Shape()
+        shape = OrtShape()
         shape.set((3, 4))
         self.assertEqual(repr(shape), "Shape((3, 4))")
         self.assertEqual(len(shape), 2)
