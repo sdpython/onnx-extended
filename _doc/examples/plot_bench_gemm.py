8a68a42d1edf14c4cae97304e2a81c25327ece75
xadupre <xadupre@microsoft.com>
2023-05-24 19:03:33 +0200

    documentation

--- _doc/examples/plot_bench_gemm.py
+++ _doc/examples/plot_bench_gemm.py
@@ -47,7 +47,7 @@ def create_model(mat_type=TensorProto.FLOAT, use_gemm8=False):
     C = make_tensor_value_info("C", mat_type, [None, None])
     inits = [I1]
     if use_gemm8:
-        f8 = mat_type in (TensorProto.FLOAT8E4M3FN, TensorProto.FLOATE5M2)
+        f8 = mat_type in (TensorProto.FLOAT8E4M3FN, TensorProto.FLOAT8E5M2)
         zero = from_array(numpy.array([0], dtype=numpy.float32), name="zero")
         inits.append(zero)
         if f8:
