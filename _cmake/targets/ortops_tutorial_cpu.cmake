aae81b8c0a6f390ed549aad85b7cb3e69f95dc7b
xadupre <xadupre@microsoft.com>
2023-05-25 12:44:30 +0200

    add missing files

---
+++ _cmake/targets/ortops_tutorial_cpu.cmake
@@ -0,0 +1,14 @@
+#
+# module: onnx_extended.reference.c_ops.cpu.c_op_conv_
+#
+message(STATUS "+ KERNEL onnx_extended.ortops.tutorial.cpu")
+
+ort_add_custom_op(
+  ortops_tutorial_cpu
+  "CPU"
+  ../onnx_extended/ortops/tutorial/cpu
+  ../onnx_extended/ortops/tutorial/cpu/my_kernel.cc
+  ../onnx_extended/ortops/tutorial/cpu/my_kernel_attr.cc
+  ../onnx_extended/ortops/tutorial/cpu/ort_tutorial_cpu_lib.cc)
+# needed to include helpers.h
+target_include_directories(ortops_tutorial_cpu PRIVATE "${ORTAPI_INCLUDE_DIR}/")
---
+++ _cmake/targets/ortops_tutorial_cuda.cmake
@@ -0,0 +1,13 @@
+#
+# module: onnx_extended.reference.c_ops.cpu.c_op_conv_
+#
+message(STATUS "+ KERNEL onnx_extended.ortops.tutorial.cuda")
+
+ort_add_custom_op(
+  ortops_tutorial_cuda
+  "CUDA"
+  ../onnx_extended/ortops/tutorial/cuda
+  ../onnx_extended/ortops/tutorial/cuda/custom_gemm.cc
+  ../onnx_extended/ortops/tutorial/cpu/ort_tutorial_cuda_lib.cc)
+# needed to include helpers.h
+target_include_directories(ortops_tutorial_cpu PRIVATE "${ORTAPI_INCLUDE_DIR}/")
