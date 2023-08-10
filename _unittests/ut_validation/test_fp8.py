f7c6d4069c9749ac7e58d9cef8c95b8a081dab02
Xavier Dupre <xadupre@microsoft.com>
2023-08-10 09:40:22 +0200

    fix import issue

--- _unittests/ut_validation/test_fp8.py
+++ _unittests/ut_validation/test_fp8.py
@@ -2,6 +2,12 @@ import unittest
 import numpy
 from onnx_extended.ext_test_case import ExtTestCase
 
+try:
+    from onnx_array_api.validation.f8 import search_float32_into_fe4m3
+except ImportError:
+    # onnx-array-api is not recent enough
+    search_float32_into_fe4m3 = None
+
 
 class TestFloat8(ExtTestCase):
     def test_cast_float32_to_e4m3fn(self):
@@ -16,9 +22,11 @@ class TestFloat8(ExtTestCase):
         f82 = cast_float32_to_e4m3fn(back)
         self.assertEqualArray(f8, f82)
 
+    @unittest.skipIf(
+        search_float32_into_fe4m3 is None, reason="onnx-array-api not recent enough"
+    )
     def test_cast_float32_to_e4m3fn_more(self):
         from onnx_extended.validation.cython.fp8 import cast_float32_to_e4m3fn
-        from onnx_array_api.validation.f8 import search_float32_into_fe4m3
 
         vect_search_float32_into_fe4m3 = numpy.vectorize(search_float32_into_fe4m3)
 
