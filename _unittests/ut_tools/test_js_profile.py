4cc33e5e3bf3e0250467b0271a36b6a6dfc1efea
Xavier Dupre <xadupre@microsoft.com>
2023-09-12 12:48:07 +0200

    add function to plot a profile

---
+++ _unittests/ut_tools/test_js_profile.py
@@ -0,0 +1,223 @@
+import os
+import unittest
+import numpy as np
+from onnx import TensorProto
+from onnx.checker import check_model
+from onnx.helper import (
+    make_model,
+    make_graph,
+    make_node,
+    make_opsetid,
+    make_tensor_value_info,
+)
+from onnx.numpy_helper import from_array
+from onnxruntime import InferenceSession, SessionOptions
+import matplotlib.pyplot as plt
+from onnx_extended.ext_test_case import ExtTestCase
+from onnx_extended.tools.js_profile import js_profile_to_dataframe, plot_ort_profile
+
+
+class TestJsProfile(ExtTestCase):
+    def _get_model(self):
+        model_def0 = make_model(
+            make_graph(
+                [
+                    make_node("Add", ["X", "init1"], ["X1"]),
+                    make_node("Add", ["X", "init2"], ["X2"]),
+                    make_node("Add", ["X", "init3"], ["inter"]),
+                    make_node("Mul", ["X1", "inter"], ["Xm"]),
+                    make_node("Sub", ["X2", "Xm"], ["final"]),
+                ],
+                "test",
+                [make_tensor_value_info("X", TensorProto.FLOAT, [None])],
+                [make_tensor_value_info("final", TensorProto.FLOAT, [None])],
+                [
+                    from_array(np.array([1], dtype=np.float32), name="init1"),
+                    from_array(np.array([2], dtype=np.float32), name="init2"),
+                    from_array(np.array([3], dtype=np.float32), name="init3"),
+                ],
+            ),
+            opset_imports=[make_opsetid("", 18)],
+        )
+        check_model(model_def0)
+        return model_def0
+
+    def test_js_profile_to_dataframe(self):
+        sess_options = SessionOptions()
+        sess_options.enable_profiling = True
+        sess = InferenceSession(
+            self._get_model().SerializeToString(),
+            sess_options,
+            providers=["CPUExecutionProvider"],
+        )
+        for _ in range(11):
+            sess.run(None, dict(X=np.arange(10).astype(np.float32)))
+        prof = sess.end_profiling()
+
+        df = js_profile_to_dataframe(prof, first_it_out=True)
+        self.assertEqual(df.shape, (189, 19))
+        self.assertEqual(
+            list(df.columns),
+            [
+                "cat",
+                "pid",
+                "tid",
+                "dur",
+                "ts",
+                "ph",
+                "name",
+                "args_op_name",
+                "op_name",
+                "args_thread_scheduling_stats",
+                "args_output_type_shape",
+                "args_output_size",
+                "args_parameter_size",
+                "args_activation_size",
+                "args_node_index",
+                "args_input_type_shape",
+                "args_provider",
+                "event_name",
+                "iteration",
+            ],
+        )
+
+        df = js_profile_to_dataframe(prof, agg=True)
+        self.assertEqual(df.shape, (15, 1))
+        self.assertEqual(list(df.columns), ["dur"])
+
+        df = js_profile_to_dataframe(prof, agg_op_name=True)
+        self.assertEqual(df.shape, (189, 19))
+        self.assertEqual(
+            list(df.columns),
+            [
+                "cat",
+                "pid",
+                "tid",
+                "dur",
+                "ts",
+                "ph",
+                "name",
+                "args_op_name",
+                "op_name",
+                "args_thread_scheduling_stats",
+                "args_output_type_shape",
+                "args_output_size",
+                "args_parameter_size",
+                "args_activation_size",
+                "args_node_index",
+                "args_input_type_shape",
+                "args_provider",
+                "event_name",
+                "iteration",
+            ],
+        )
+
+        os.remove(prof)
+
+    def test_plot_profile_2(self):
+        sess_options = SessionOptions()
+        sess_options.enable_profiling = True
+        sess = InferenceSession(
+            self._get_model().SerializeToString(),
+            sess_options,
+            providers=["CPUExecutionProvider"],
+        )
+        for _ in range(11):
+            sess.run(None, dict(X=np.arange(10).astype(np.float32)))
+        prof = sess.end_profiling()
+
+        df = js_profile_to_dataframe(prof, first_it_out=True)
+
+        fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+        plot_ort_profile(df, ax[0], ax[1], "test_title")
+        # fig.savefig("graph1.png")
+        self.assertNotEmpty(fig)
+
+        os.remove(prof)
+
+    def test_plot_profile_agg(self):
+        sess_options = SessionOptions()
+        sess_options.enable_profiling = True
+        sess = InferenceSession(
+            self._get_model().SerializeToString(),
+            sess_options,
+            providers=["CPUExecutionProvider"],
+        )
+        for _ in range(11):
+            sess.run(None, dict(X=np.arange(10).astype(np.float32)))
+        prof = sess.end_profiling()
+
+        df = js_profile_to_dataframe(prof, first_it_out=True, agg=True)
+
+        fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+        plot_ort_profile(df, ax, title="test_title")
+        fig.tight_layout()
+        # fig.savefig("graph2.png")
+        self.assertNotEmpty(fig)
+
+        os.remove(prof)
+
+    def _get_model_domain(self):
+        model_def0 = make_model(
+            make_graph(
+                [
+                    make_node("Transpose", ["X"], ["Xt"], perm=[1, 0]),
+                    make_node(
+                        "CustomGemmFloat",
+                        ["X", "Xt"],
+                        ["final"],
+                        domain="onnx_extented.ortops.tutorial.cpu",
+                    ),
+                ],
+                "test",
+                [make_tensor_value_info("X", TensorProto.FLOAT, [None, None])],
+                [make_tensor_value_info("final", TensorProto.FLOAT, [None, None])],
+            ),
+            opset_imports=[
+                make_opsetid("", 18),
+                make_opsetid("onnx_extented.ortops.tutorial.cpu", 1),
+            ],
+        )
+        check_model(model_def0)
+        return model_def0
+
+    def test_plot_domain_agg(self):
+        from onnx_extended.ortops.tutorial.cpu import get_ort_ext_libs
+
+        sess_options = SessionOptions()
+        sess_options.enable_profiling = True
+        sess_options.register_custom_ops_library(get_ort_ext_libs()[0])
+        sess = InferenceSession(
+            self._get_model_domain().SerializeToString(),
+            sess_options,
+            providers=["CPUExecutionProvider"],
+        )
+        for _ in range(11):
+            sess.run(None, dict(X=np.arange(16).astype(np.float32).reshape((-1, 4))))
+        prof = sess.end_profiling()
+
+        df = js_profile_to_dataframe(prof, first_it_out=True)
+        print(df)
+        print(df.tail(5).T)
+
+        fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+        plot_ort_profile(df, ax, title="test_title")
+        fig.tight_layout()
+        # fig.savefig("graph3.png")
+        self.assertNotEmpty(fig)
+
+        os.remove(prof)
+
+
+if __name__ == "__main__":
+    import logging
+
+    for name in [
+        "matplotlib.font_manager",
+        "PIL.PngImagePlugin",
+        "matplotlib",
+        "matplotlib.pyplot",
+    ]:
+        log = logging.getLogger(name)
+        log.setLevel(logging.ERROR)
+    unittest.main(verbosity=2)
---
+++ onnx_extended/tools/js_profile.py
@@ -0,0 +1,173 @@
+import json
+from typing import List, Optional, Union
+from pandas import DataFrame
+
+
+def post_process_df_profile(
+    df: DataFrame,
+    first_it_out: bool = False,
+    agg: bool = False,
+    agg_op_name: bool = True,
+) -> DataFrame:
+    """
+    Post-processed a dataframe obtained after profiling onnxruntime.
+    It adds a column for a more explicit event name and adds
+    a column for the iteration number
+
+    :param agg: aggregate the result
+    :param first_it_out: leave the first iteration
+        out of the aggregation
+    :param agg_op_name: aggregate on operator name or operator index
+    :return: DataFrame
+    """
+    events = {"kernel_time", "fence_after", "fence_before"}
+
+    def sep_event(s):
+        for e in events:
+            if s.endswith(e):
+                return e
+        return s
+
+    df = df.copy()
+    df["event_name"] = df["name"].apply(sep_event)
+    df["iteration"] = -1
+    current = -1
+    for i in range(df.shape[0]):
+        if df.loc[i, "name"] == "SequentialExecutor::Execute":
+            current += 1
+        df.loc[i, "iteration"] = current
+
+    if not agg:
+        return df
+
+    agg_cols = ["cat", "args_node_index", "args_op_name", "args_provider", "event_name"]
+    if first_it_out:
+        df["it==0"] = (df["iteration"] <= 0).astype(int)
+        agg_cols.insert(0, "it==0")
+    if agg_op_name:
+        del agg_cols[agg_cols.index("args_node_index")]
+    for c in agg_cols:
+        df[c] = df[c].fillna("")
+    df["dur"] = df["dur"].fillna(0)
+    agg = df[agg_cols + ["dur"]].groupby(agg_cols).sum()
+    return agg
+
+
+def js_profile_to_dataframe(
+    filename: str,
+    as_df: bool = True,
+    first_it_out: bool = False,
+    agg: bool = False,
+    agg_op_name: bool = False,
+) -> Union[List, DataFrame]:
+    """
+    Profiles the execution of an onnx graph with onnxruntime.
+
+    :param filename: filename holding the profiling stored in json format
+    :param as_df: returns the
+    :param first_it_out: if aggregated, leaves the first iteration out
+    :param agg: aggregate by event
+    :param agg_op_name: aggregate on operator name or operator index
+    :return: DataFrame or dictionary
+    """
+    with open(filename, "r") as f:
+        content = f.read()
+    js = json.loads(content)
+
+    suffixes = ["_kernel_time", "_fence_before", "_fence_after"]
+    rows = []
+    for row in js:
+        if "args" in row and isinstance(row["args"], dict):
+            for k, v in row["args"].items():
+                row[f"args_{k}"] = v
+            del row["args"]
+        name = row["name"]
+        for suf in suffixes:
+            if name.endswith(suf):
+                changed = name[: -len(suf)]
+                row["op_name"] = changed
+                break
+        rows.append(row)
+    if as_df:
+        return post_process_df_profile(
+            DataFrame(rows), first_it_out=first_it_out, agg=agg, agg_op_name=agg_op_name
+        )
+    return rows
+
+
+def plot_ort_profile(
+    df: DataFrame,
+    ax0: Optional["matplotlib.axes.Axes"] = None,
+    ax1: Optional["matplotlib.axes.Axes"] = None,
+    title: Optional[str] = None,
+) -> "matplotlib.axes.Axes":
+    """
+    Plots time spend in computation based on dataframe
+    produced by function :func:`js_profile_to_dataframe`.
+
+    :param df: dataframe
+    :param ax0: first axis to draw time
+    :param ax1: second axis to draw occurences
+    :param title: graph title
+    :return: the graph
+    """
+    if ax0 is None:
+        import matplotlib as plt
+
+        ax0 = plt.gca()  # pragma: no cover
+
+    if "args_provider" in df.columns:
+        # Aggregation by operator
+        df = df.copy()
+        df["args_provider"] = df["args_provider"].apply(
+            lambda s: s.replace("ExecutionProvider", "") if isinstance(s, str) else s
+        )
+        gr_dur = (
+            df[["dur", "args_op_name", "args_provider"]]
+            .groupby(["args_provider", "args_op_name"])
+            .sum()
+            .sort_values("dur")
+        )
+        gr_dur.plot.barh(ax=ax0)
+        ax0.get_yaxis().set_label_text("")
+        ax0.set_yticklabels(ax0.get_yticklabels(), rotation=45, ha="right")
+        if title is not None:
+            ax0.set_title(title)
+        if ax1 is not None:
+            gr_n = (
+                df[["dur", "args_op_name", "args_provider"]]
+                .groupby(["args_provider", "args_op_name"])
+                .count()
+                .sort_values("dur")
+            )
+            gr_n = gr_n.loc[gr_dur.index, :]
+            gr_n.plot.barh(ax=ax1)
+            ax1.set_title("n occurences")
+            ax1.get_yaxis().set_label_text("")
+            ax1.set_yticklabels(ax1.get_yticklabels(), rotation=45, ha="right")
+        return ax0
+
+    df = df.reset_index(drop=False).copy()
+    df["args_provider"] = df["args_provider"].apply(
+        lambda s: s.replace("ExecutionProvider", "") if isinstance(s, str) else s
+    )
+    df = df[
+        (df["it==0"] == 0) & (df["cat"] == "Node") & (df["event_name"] == "kernel_time")
+    ]
+    df = (
+        df[["args_node_index", "args_op_name", "args_provider", "dur"]]
+        .groupby(
+            [
+                "args_node_index",
+                "args_provider",
+                "args_op_name",
+            ]
+        )
+        .sum()
+    )
+    df = df.sort_index(ascending=False)
+    df.plot.barh(ax=ax0)
+    ax0.get_yaxis().set_label_text("")
+    if title is not None:
+        ax0.set_title(title)
+    return ax0
