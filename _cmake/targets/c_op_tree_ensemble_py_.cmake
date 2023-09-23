4b9e8c68d221fa4ebddd4f68245687cd247b3a2b
Xavier Dupre <xadupre@microsoft.com>
2023-09-24 00:18:51 +0200

    link

--- _cmake/targets/c_op_tree_ensemble_py_.cmake
+++ _cmake/targets/c_op_tree_ensemble_py_.cmake
@@ -7,7 +7,7 @@ local_pybind11_add_module(
   c_op_tree_ensemble_py_ OpenMP::OpenMP_CXX
   ../onnx_extended/reference/c_ops/cpu/c_op_tree_ensemble_py_.cpp)
 
-target_link_libraries(c_op_tree_ensemble_py_ PRIVATE common_kernels)
+target_link_libraries(c_op_tree_ensemble_py_ PRIVATE common_kernels common)
 
 target_include_directories(c_op_tree_ensemble_py_ PRIVATE ${ROOT_INCLUDE_PATH})
 
