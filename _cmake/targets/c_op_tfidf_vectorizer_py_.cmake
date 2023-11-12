f47863a2672d4840b9b6befa94d42e131c4d573b
Xavier Dupré <xadupre@users.noreply.github.com>
2023-11-13 00:50:18 +0100

    Adds c implementation of tfidfvectorizer and python implementation of tokenizer (#111)
    * Add implementation of tfidfvectorizer
    
    * changelogs
    
    * fix tfidf
    
    * fix cmake
    
    * add missing header
    
    * fix compiltation
    
    * removes tests on mac
    
    * disable one unstable unittest
    
    * crash

---
+++ _cmake/targets/c_op_tfidf_vectorizer_py_.cmake
@@ -0,0 +1,13 @@
+#
+# module: onnx_extended.reference.c_ops.cpu.c_op_tree_ensemble_py_
+#
+message(STATUS "+ PYBIND11 onnx_extended.reference.c_ops.cpu.c_op_tfidf_vectorizer_py_")
+
+local_pybind11_add_module(
+  c_op_tfidf_vectorizer_py_ OpenMP::OpenMP_CXX
+  ../onnx_extended/reference/c_ops/cpu/c_op_tfidf_vectorizer_py_.cpp)
+
+target_link_libraries(c_op_tfidf_vectorizer_py_ PRIVATE common_kernels common)
+
+target_include_directories(c_op_tfidf_vectorizer_py_ PRIVATE ${ROOT_INCLUDE_PATH})
+
