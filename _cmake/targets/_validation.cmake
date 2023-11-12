db4d543a4b1562b0db6edaa466ef56db2ffc774e
Xavier Dupre <xadupre@microsoft.com>
2023-11-12 19:25:17 +0100

    fix compiltations issues

--- _cmake/targets/_validation.cmake
+++ _cmake/targets/_validation.cmake
@@ -6,17 +6,17 @@ message(STATUS "+ PYBIND11 onnx_extended.validation.cpu._validation")
 add_library(lib_validation_cpp STATIC
   ../onnx_extended/validation/cpu/murmur_hash3.cpp
   ../onnx_extended/validation/cpu/speed_metrics.cpp
-  ../onnx_extended/validation/cpu/vector_function.cpp
-  ../onnx_extended/validation/cpu/vector_sparse.cpp)
+  ../onnx_extended/validation/cpu/vector_function.cpp)
 target_compile_definitions(lib_validation_cpp PRIVATE PYTHON_MANYLINUX=${PYTHON_MANYLINUX})
-target_include_directories( lib_validation_cpp PRIVATE "${ROOT_INCLUDE_PATH}")
 set_property(TARGET lib_validation_cpp PROPERTY POSITION_INDEPENDENT_CODE ON)
 
 local_pybind11_add_module(
   _validation OpenMP::OpenMP_CXX
   ../onnx_extended/validation/cpu/_validation.cpp
+  ../onnx_extended/validation/cpu/vector_sparse.cpp
   ../onnx_extended/validation/cpu/vector_sum.cpp)
 message(STATUS "    LINK _validation <- lib_validation_cpp")
+target_include_directories(_validation PRIVATE "${ROOT_INCLUDE_PATH}")
 target_link_libraries(_validation PRIVATE lib_validation_cpp)
 
 add_executable(
