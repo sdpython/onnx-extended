9c6a70953ff3871ab83349a977e6c16894c04243
Xavier Dupré <xadupre@users.noreply.github.com>
2023-11-12 18:52:46 +0100

    Upgrade onnxruntime version to 1.16.2 (#112)
    * Upgrade onnxruntime version to 1.16.2
    
    * changelogs
    
    * fix version
    
    * fix darwin issue

--- onnx_extended/__init__.py
+++ onnx_extended/__init__.py
@@ -35,7 +35,7 @@ def _check_installation_ortcy(onnx_model, verbose):
 
         this = os.path.dirname(cyfile)
         files = os.listdir(this)
-        if "libonnxruntime.so.1.16.1" in files:
+        if "libonnxruntime.so.1.16.2" in files:
             if verbose:
                 local_print(
                     "[check_installation_ortcy] weird issue as the "
