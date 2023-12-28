7717cd2f7698fcc204cddf7fddcc4b7bd52ddeb8
Xavier Dupre <xadupre@microsoft.com>
2023-12-28 21:37:05 +0100

    cleaning

--- _cmake/targets/_validation_cuda_example_py.cmake
+++ _cmake/targets/_validation_cuda_example_py.cmake
@@ -7,12 +7,9 @@ if(CUDA_AVAILABLE)
 
   cuda_pybind11_add_module(
     cuda_example_py
-    ../onnx_extended/validation/cuda/cuda_example_py.cpp
     ../onnx_extended/validation/cuda/cuda_fpemu.cu
     ../onnx_extended/validation/cuda/cuda_tensor.cu
-    ../onnx_extended/validation/cuda/cuda_gemm.cu
-    ../onnx_extended/validation/cuda/cuda_example.cu
-    ../onnx_extended/validation/cuda/cuda_example_reduce.cu)
+    ../onnx_extended/validation/cuda/cuda_gemm.cu)
 
   target_include_directories(cuda_example_py PRIVATE ${ROOT_INCLUDE_PATH})
   target_link_libraries(cuda_example_py PRIVATE common)
