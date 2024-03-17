0cff76cd40742668d929f4a50a77f2bcca3b4b97
Xavier Dupre <xadupre@microsoft.com>
2024-03-17 18:56:29 +0100

    update

--- clean_onnx.sh
+++ clean_onnx.sh
@@ -1,4 +1,9 @@
 rm _doc/tutorial/*.json
-rm an_onnx_model.onnx
 rm *.onnx
+rm *.data
 rm *.png
+rm .build_path.txt
+
+rm _doc/examples/plot*.onnx
+rm _doc/examples/plot*.png
+rm _doc/examples/plot*.csv
