15ded26b2769c1d10054d4f179d1051a04d34203
Xavier Dupre <xadupre@microsoft.com>
2024-04-17 10:31:59 +0200

    Upgrade to onnxruntime==1.17.3

--- setup.py
+++ setup.py
@@ -730,7 +730,7 @@ def get_ext_modules():
 # beginning of setup
 ######################
 
-DEFAULT_ORT_VERSION = "1.17.1"
+DEFAULT_ORT_VERSION = "1.17.3"
 here = os.path.dirname(__file__)
 if here == "":
     here = "."
