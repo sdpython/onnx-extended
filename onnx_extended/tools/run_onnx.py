a1a2bfc6b58ea73625e59b194d6348960084f1bd
Xavier Dupre <xadupre@microsoft.com>
2024-05-29 14:39:59 +0200

    Upgrade to onnxruntime==1.18.0

--- onnx_extended/tools/run_onnx.py
+++ onnx_extended/tools/run_onnx.py
@@ -394,6 +394,7 @@ def bench_virtual(
     if modules is None:
         # ext = "https://github.com/sdpython/onnx-extended.git"
         modules = [
+            {"onnxruntime": "1.18.0", "onnx": None, "onnx-extended": "0.3.0"},
             {"onnxruntime": "1.17.1", "onnx": None, "onnx-extended": "0.2.3"},
             {
                 "onnxruntime": "1.16.3",
